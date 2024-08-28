d1d624fd96368b0d1097be88d9073fd1a25b2e9b
BhanuPrakash43 <bhanu742630@gmail.com>
2024-08-28 23:19:59 +0530

    logout functionalities added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import ThemeSwithcher from "./ThemeSwitcher";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  function handleLogout() {
+    logout();
+    navigate("/login");
+  }
+
   return (
     <div className="h-16 w-screen bg-white dark:bg-[#1F1F1F] fixed px-14 text-[#5B5F66] dark:text-white top-0 flex justify-between items-center border-b-2 dark:border-[#343A40] border-[#E0E0E0]">
       <div className="text-xl">Onebox</div>
 
       <div className="flex items-center">
-        <ThemeSwithcher />
+        <ThemeSwitcher />
+
+        <button
+          onClick={handleLogout}
+          className="flex items-center border-2 border-red-500 rounded-md py-1 px-3 text-red-500 text-xl mr-10 cursor-pointer"
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
